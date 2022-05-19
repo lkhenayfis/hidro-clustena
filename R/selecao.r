615bd597509e49ed511b7ff64d705012bbc417af
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-05-19 12:16:56 -0300

    Add documentacao func selecpor*

--- R/selecao.r
+++ R/selecao.r
@@ -1,5 +1,30 @@
 ######################### FUNCOES PARA CLUSTERIZAR E/OU SELECIONAR CENARIOS ########################
 
+#' Selecao De Cenarios Por Quantil
+#' 
+#' Seleciona cenarios representativos buscando aqueles mais proximos de quantis especificados
+#' 
+#' A selecao por quantis requer que cada cenario seja reduzido a apenas uma dimensao (de modo que 
+#' seja possivel calcular quantis unicos). A compactacao pode ser feita por qualquer funcao que
+#' retorne objetos da classe \code{compactcen}, informada atraves do argumento \code{compact_fun}. A
+#' funcao sera chamada com \code{cenarios} como primeiro arumento; \code{compact_args} permite 
+#' informar demais parametros a serem utilizados por \code{compact_fun}, na forma de uma lista 
+#' nomeada.
+#' 
+#' O argumento cenarios deve ser um objeto da classe \code{cenariosena} contendo apenas um ano de 
+#' referencia e bacia. \code{quantis} deve ser um vetor indicando quais quantis estao sendo 
+#' buscados. Por construcao e impossivel extrair exatamente estes quantis, de modo que serao 
+#' retornados os indices dos cenarios associados aos quantis mais proximos. 
+#' 
+#' @param cenarios objeto da classe \code{cenariosena} do qual selecionar cenarios representativos
+#' @param quantis vetor de quantis para selecionar
+#' @param compact_fun funcao para compactacao dos cenarios. Ver \code{\link{compact_funs}}
+#' @param compact_args lista nomeada informando argumentos (alem do primeiro) a \code{compact_fun}
+#' 
+#' @return vetor de inteiros indicando indices dos cenarios selecionados
+#' 
+#' @export
+
 selecporquantil <- function(cenarios, quantis = c(.25, .5, .75), compact_fun = acumulaena,
     compact_args = list()) {
 
@@ -16,6 +41,46 @@ selecporquantil <- function(cenarios, quantis = c(.25, .5, .75), compact_fun = a
 
     return(maisprox)
 }
+
+#' Selecao Ou Extracao De Cenarios Por Clusterizacao
+#' 
+#' Seleciona ou extrai cenarios baseado nos centroides da clusterizacao de cenarios
+#' 
+#' O procedimento desta funcao e aplicar a reducao de dimensionalidade em \code{cenarios} e 
+#' clusterizar as variaveis no espaco reduzido em \code{nc} clusters.
+#' 
+#' A compactacao pode ser feita por qualquer funcao que retorne objetos da classe \code{compactcen},
+#' informada atraves do argumento \code{compact_fun}. A funcao sera chamada com \code{cenarios} como
+#' primeiro arumento; \code{compact_args} permite informar demais parametros a serem utilizados por
+#' \code{compact_fun}, na forma de uma lista nomeada.
+#' 
+#' A clusterizacao em si e realizada por uma chamada a funcao passada pelo argumento 
+#' \code{clust_fun}, uma funcao que deve receber como primeiro argumento um objeto \code{cenarios} e 
+#' como segundo argumento o numero de clusters desejado \code{nc}, e retornar um objeto com os 
+#' metodos \code{getclustclass} e \code{getclustmeans}, para retornar as classificacoes de cada 
+#' cenario e as medias dos centroides. A funcao passada sera usada numa chamada da forma 
+#' \code{clust_fun(cenarios, nc, ...)}. \code{compact_fun} e \code{compact_args}.
+#' 
+#' Por fim, \code{transforma} controla o que sera retornado. Se \code{FALSE}, a funcao entrega 
+#' apenas os indices dos cenarios mais proximos, por distancia euclidiana, dos centroides de cada
+#' cluster. Se \code{TRUE}, o centroide de cada cluster (no espaco reduzido) sera transformado de
+#' volta para o espaco original. Isso so e possivel caso a compactacao utilizada permita este tipo
+#' de operacao: por exemplo, a reducao de dimensionalidade por acumulo de enas parciais e uma 
+#' transformacao que nao permite retornar para a escala original. Outras como PCA e autoencoder sim.
+#' 
+#' @param cenarios objeto da classe \code{cenariosena} do qual selecionar cenarios representativos
+#' @param nc numero de clusters
+#' @param clust_fun funcao para clusterizacao. Veja \code{\link{clust_funs}} para mais detalhes
+#' @param ... demais parametros passados a \code{clust_fun}
+#' @param transforma booleano indicando se devem ser retornados indices dos cenarios mais proximos
+#'     do centroide de cada cluster (\code{FALSE}) ou se o cetroide deve ser transformado de volta
+#'     para a escala original (\code{TRUE}). Ver Detalhes
+#' @param compact_fun funcao para compactacao dos cenarios. Ver \code{\link{compact_funs}}
+#' @param compact_args lista nomeada informando argumentos (alem do primeiro) a \code{compact_fun}
+#' 
+#' @return vetor de inteiros indicando indices dos cenarios selecionados
+#' 
+#' @export
 
 selecporcluster <- function(cenarios, nc, clust_fun = clustkmeans, ..., transforma = FALSE,
     compact_fun = PCAena, compact_args = list()) {
